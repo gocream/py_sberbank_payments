2b47a224558cd2dfd24b1b99a5c61c45e35a56ab
Dmitry Dobrynin <dd@tovarisch.engineer>
2023-11-05 00:26:43 +0100

    🔧 (chore): Library Configuration

---
+++ py_sberbank_payments/__init__.py
@@ -0,0 +1,24 @@
+from py_sberbank_payments.http import SberbankPaymentApi
+
+
+__version__ = "0.1.9"
+"""
+Version according to [semver](https://semver.org/).
+
+To update the version, use [hatch version](https://hatch.pypa.io/latest/version/#updating)
+
+show current version:
+
+```console
+$ hatch version
+1.0.0a0
+```
+
+update patch and beta version from `1.0.0a0` to `1.0.1b0`
+
+```console
+$ hatch version patch,beta
+Old: 1.0.0a0
+New: 1.0.1b0
+```
+"""
